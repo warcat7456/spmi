5432284f81bc9ff6e076a5d6441b4c15a6eb98c9
Recky Anderson <reckyanderson@gmail.com>
2023-10-19 23:24:42 +0700

    Menambahkan fungsi return view about page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,11 @@ class HomeController extends Controller
         ]);
     }
 
+    public function about()
+    {
+        return view('home.about');
+    }
+
     public function berkas(Element $element)
     {
         $att = Berkas::where('element_id', $element->id)->get();
