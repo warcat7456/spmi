327c5098084306bde7a972bcdad2746e1ebcbdd8
Recky Anderson <reckyanderson@gmail.com>
2023-10-19 22:49:21 +0700

    hapus syntax komen

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use App\Jenjang;
 use App\Prodi;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
-
 //Auth
 Route::get('login', 'AuthController@index')->name('login');
 Route::post('proses', 'AuthController@proses');
