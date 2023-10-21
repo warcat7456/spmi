e2a8b28685f37cfe7db2cb417800844c69b610fd
Ugik DEV <ugik.dev@gmail.com>
2023-10-21 22:50:11 +0700

    home base dosen

--- resources/views/template/BaseView.blade.php
+++ resources/views/template/BaseView.blade.php
@@ -91,7 +91,7 @@
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }} - {{ Auth::user()->prodi_name }}</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }} {{ Auth::user()->role =="Dosen" ? '-'.Auth::user()->prodi_name : '' }}</span>
                                 <img class="img-profile rounded-circle" src="{{ asset('portal/img/undraw_profile.svg') }}">
                             </a>
                             <!-- Dropdown - User Information -->
