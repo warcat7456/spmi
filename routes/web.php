c299ac796cdc49f5bc10478eb84deac5b7944b51
Recky Anderson <reckyanderson@gmail.com>
2023-10-29 01:34:40 +0700

    tess

--- routes/web.php
+++ routes/web.php
@@ -221,3 +221,6 @@ Route::post('dropdownlist/getl1nu', 'dropdownController@getL1nu')->name('l1nu');
 Route::post('dropdownlist/getl2nu', 'dropdownController@getL2nu')->name('l2nu');
 Route::post('dropdownlist/getl3nu', 'dropdownController@getL3nu')->name('l3nu');
 Route::post('dropdownlist/getl4nu', 'dropdownController@getL4nu')->name('l4nu');
+//
+
+//
