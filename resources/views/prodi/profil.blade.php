fb5f3a84cdfc5dc18080c342128be02364a52243
Ugik DEV <ugik.dev@gmail.com>
2023-11-08 05:02:29 +0700

    profil view

--- resources/views/prodi/profil.blade.php
+++ resources/views/prodi/profil.blade.php
@@ -4,44 +4,50 @@
 @endsection
 
 @section('content')
-    <div class="bg-white py-3 px-4">
-        <div class="row">
-            <div class="col-md-8">
-                <h1 class=" font-weight-bold text-info">
-                    <u>Program Studi {{ $p->name }}</u>
-                </h1>
-                <span class="border border-info py-2 px-2 rounded my-1 d-inline-block">Profil</span>
-                @if ($p->kode == Auth::user()->prodi_kode && $edit == true)
-                    <a class="btn btn-info py-2 px-2 rounded my-1 d-inline-block" href="{{ url('edit-profil-prodi') }}"><i
-                            class="fa fa-pencil"></i> Edit Profil</a>
-                @endif
-                <div class="my-3" style="word-wrap: break-word">
-                    {!! $p->deskripsi !!}
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mb-3">
-                    <img src="{{ $p->foto ? route('showimage', ['filename' => $p->foto]) : asset('home/img/about.png') }}"
-                        alt="Deskripsi Gambar">
-                </div>
-
-                <!-- Visi -->
-                <div class="my-2 py-2 text-center">
-                    <span class="border border-info py-2 px-4 rounded my-1 d-inline-block mx-auto">Visi</span>
-                    <div class="my-3" style="word-wrap: break-word">
-                        {!! $p->visi !!}
+<div class="row">
+    <div class="col-md-9">
+        <div class="card">
+            <div class="card-boy">
+                <div class="col-md-12 mt-3">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img style="width: 100%; border-radius : calc(0.35rem - 1px)" src="{{ $p->foto ? route('showimage', ['filename' => $p->foto]) : asset('home/img/about.png') }}">
+                        </div>
+                        <div class="col-md-9 d-flex align-items-center">
+                            <h1 class="d-flex align-items-center font-weight-bold text-info">
+                                <u>Program Studi {{ $p->name }}</u>
+                            </h1>
+                        </div>
                     </div>
                 </div>
-                <!-- Misi -->
-                <div class="my-2 py-2 text-center">
-                    <span class="border border-info py-2 px-4 rounded my-1 d-inline-block mx-auto">Misi</span>
-                    <div class="my-3" style="word-wrap: break-word">
-                        {!! $p->misi !!}
+                <div class="col-md-12">
+                    <span class="border border-info py-2 px-2 rounded my-1 d-inline-block">Profil</span>
+                    @if ($p->kode == Auth::user()->prodi_kode && $edit == true)
+                    <a class="btn btn-info py-2 px-2 rounded my-1 d-inline-block" href="{{ url('edit-profil-prodi') }}"><i class="fa fa-pencil"></i> Edit Profil</a>
+                    @endif
+                    <div class="my-3 text-justify" style="word-wrap: break-word">
+                        {!! $p->deskripsi !!}
                     </div>
-
+                </div>
+                <div class="col-md-12">
+                    <div class="my-2 py-2 text-center">
+                        <span class="border border-info py-2 px-4 rounded my-1 d-inline-block mx-auto">Visi</span>
+                        <div class="my-3 text-justify" style="word-wrap: break-word ">
+                            {!! $p->visi !!}
+                        </div>
+                    </div>
+                    <div class="my-2 py-2 text-center">
+                        <span class="border border-info py-2 px-4 rounded my-1 d-inline-block mx-auto">Misi</span>
+                        <div class="my-3 text-justify" style="word-wrap: break-word">
+                            {!! $p->misi !!}
+                        </div>
+                    </div>
                 </div>
             </div>
-
-
         </div>
-    @endsection
+    </div>
+    <div class="col-md-3 card">
+        <span class="border border-info py-2 px-2 rounded my-1 d-inline-block mt-3"><strong>Berkas Akreditasi</strong></span>
+    </div>
+</div>
+@endsection
