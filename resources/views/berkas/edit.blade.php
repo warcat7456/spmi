3498aab4464e876b2b85f22f08d388775d0a0a1d
Recky Anderson <reckyanderson@gmail.com>
2023-11-15 21:30:53 +0700

    input tipe file harus dibatasi extensi yang diperbolehkan

--- resources/views/berkas/edit.blade.php
+++ resources/views/berkas/edit.blade.php
@@ -27,7 +27,7 @@
                         </div>
                         <div class="form-group">
                             <label>Berkas</label>
-                            <input type="file" class="form-control" name="file">
+                            <input type="file" accept=".pdf,.docx,.csv,.xls,.xlsx" class="form-control" name="file">
                             <small id="helpId" class="text-muted">Upload Berkas</small>
                         </div>
                         <hr>
